c2cadb6be39c7b37ba28f97b519ba85eafd15ba3
Yuriy Samchuk <msdr.main@gmail.com>
2025-05-19 21:40:08 +0300

    new design: страницы Пополнение баланса; Баланс

--- js/cabinet.js
+++ js/cabinet.js
@@ -16,4 +16,27 @@
             cabinetRightMenu.classList.toggle('isVisible');
         });
     });
+
+    const balanceUpdateButtons = document.querySelectorAll('.js-balance-update');
+
+    if (balanceUpdateButtons.length > 0) {
+        const balanceUpdateFields = document.querySelectorAll('.js-balance-update-field');
+
+        if (balanceUpdateFields.length > 0) {
+
+            console.log({balanceUpdateFields});
+
+            balanceUpdateButtons.forEach((element) => {
+                element.addEventListener('click', (e) => {
+                    e.preventDefault();
+
+                    const value = element.dataset.value || 0;
+
+                    balanceUpdateFields.forEach((input) => {
+                        input.value = value;
+                    });
+                })
+            });
+        }
+    }
 }());
